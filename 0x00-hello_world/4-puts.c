849e1de8ce12a6ceff6c88b82e8a0d4f1ff75f5d
Musuru <musuruyakub3@gmail.com>
2023-02-16 02:58:41 -0800

    C 1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-/*
- * main - print a line of codes puts
+/**
+ * main -Print a line of codes using puts
  *
- * Return: 0
-*/ 
+ * Return:| 0
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
