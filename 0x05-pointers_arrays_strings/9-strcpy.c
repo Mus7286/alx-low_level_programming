074423619d5c884ce64be3b095f62bca5b884955
Musuru <musuruyakub3@gmail.com>
2023-02-27 04:16:23 -0800

    Task 1

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -12,15 +12,15 @@ char *_strcpy(char *dest, char *src)
 	int I = 0;
 	int x = 0;
 
-	while ((*(src + I) != '\0'))
+	while (*(src + I) != '\0')
 	{
-			I++;
+		I++;
 	}
+
 	for (; x < I ; x++)
 	{
 		dest[x] = src[x];
 	}
-
 	dest[I] = '\0';
 	return (dest);
 }
