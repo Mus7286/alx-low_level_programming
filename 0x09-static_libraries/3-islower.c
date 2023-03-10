e205a406d12fbbdecabfc59fff65cf55f3c000cb
Musuru <musuruyakub3@gmail.com>
2023-03-10 15:16:22 -0800

    Task 0

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowwercase letters
+ * @c: chr to check
+ * Return: 1 if c is lowercase, 0 otherwise
+ */
+
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
---
+++ 0x09-static_libraries/4-strbrk.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * *_strbrk - Entry point
+ * @s: input
+ * @accept: input
+ * Return: 0
+ */
+
+char *_strbrk(char *s, char *accept)
+{
+	int k;
+
+	while (*s)
+	{
+		for (k = 0; accept[k]; k++)
+		{
+			if (*s == accept[k])
+				return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
