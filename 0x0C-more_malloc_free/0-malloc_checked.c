602fb19e1ffb8d2ed92546cbd35438bfd97ed77c
Musuru <musuruyakub3@gmail.com>
2023-03-16 18:52:25 -0700

    Task 1

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,7 +4,7 @@
 
 /**
  **malloc_checked - allocates memory using malloc
- * @b: number of bytes to allocate
+ *@b: number of bytes to allocate
  *
  * Return: a pointer to the allocated memory
  */
@@ -19,5 +19,4 @@ void *malloc_checked(unsigned int b)
 		exit(98);
 
 	return (ptr);
-
 }
